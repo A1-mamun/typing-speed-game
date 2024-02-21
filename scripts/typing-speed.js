a8244507521719ac410d0b2d060c5915325d2a5a
A1-mamun <1154almamun@gmail.com>
2024-02-21 22:37:56 +0600

    all functionality done

--- scripts/typing-speed.js
+++ scripts/typing-speed.js
@@ -1,9 +1,12 @@
 function play() {
   //hide the home screen
-  hideElementByI("home-screen");
-  //show the playground
-  showElementByI("play-ground");
-  //start and continue game
+  hideElementById("home-screen");
+  hideElementById("final-score");
+  showElementById("play-ground");
+
+  setTextElementValueById("life", 5);
+  setTextElementValueById("score", 0);
+  //start and continue gam
   continueGame();
 }
 
@@ -11,4 +14,48 @@ function continueGame() {
   const alpha = getRandomAlphabet();
   //   show random alphabet to screen
   document.getElementById("alphabet").innerText = alpha;
+  setBgColor(alpha);
+}
+
+function handleKeyboardButtonPress(event) {
+  // player pressed alphabet
+  const playerPressed = event.key;
+
+  if (playerPressed === "Escape") {
+    gameOver();
+  }
+  // expected alphabet
+  const expectedAlphabet = document.getElementById("alphabet").innerText;
+  const currentAlphabet = expectedAlphabet.toLowerCase();
+
+  // check matched or not
+  if (playerPressed === currentAlphabet) {
+    const newScore = incScore();
+    setTextElementValueById("score", newScore);
+    removeBgColor(currentAlphabet);
+    continueGame();
+  } else {
+    // const currentLife = getTextElementValueById("life");
+    // const updatedLife = currentLife - 1;
+    const newLife = decLife();
+    setTextElementValueById("life", newLife);
+    if (newLife === 0) {
+      gameOver();
+    }
+  }
+}
+// ditected key press from keyboard
+document.addEventListener("keyup", handleKeyboardButtonPress);
+
+// game over
+function gameOver() {
+  hideElementById("play-ground");
+  showElementById("final-score");
+
+  const lastScore = getTextElementValueById("score");
+  setTextElementValueById("final-point", lastScore);
+
+  const currentAlphabet = getElementTextById("alphabet");
+
+  removeBgColor(currentAlphabet);
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,20 +1,64 @@
 // function for show an element
-function hideElementByI(fieldId) {
+function hideElementById(fieldId) {
   document.getElementById(fieldId).classList.add("hidden");
 }
 
 // function for show an element
-function showElementByI(fieldId) {
+function showElementById(fieldId) {
   document.getElementById(fieldId).classList.remove("hidden");
+}
+
+// add background color
+function setBgColor(fieldId) {
+  document.getElementById(fieldId).classList.add("bg-orange-400");
+}
+
+// remove bg color
+function removeBgColor(fieldId) {
+  document.getElementById(fieldId).classList.remove("bg-orange-400");
+}
+
+// increment score
+function incScore() {
+  const currentScore = getTextElementValueById("score");
+  const updatedScore = currentScore + 1;
+  return updatedScore;
+}
+
+// get text element by id
+function getTextElementValueById(fieldId) {
+  const element = document.getElementById(fieldId);
+  const elementValueText = element.innerText;
+  const value = parseInt(elementValueText);
+  return value;
+}
+
+// set text element by id
+function setTextElementValueById(fieldId, value) {
+  const element = document.getElementById(fieldId);
+  element.innerText = value;
+}
+
+function getElementTextById(elementId) {
+  const element = document.getElementById(elementId);
+  const text = element.innerText;
+  return text;
 }
 
 // function for generate random alphabet
 function getRandomAlphabet() {
   // create an alphabet array
   const alphabetString = "abcdefghijklmnopqrstuvwxyz";
-  alphabetString.split(" ");
+  const alphabets = alphabetString.split("");
 
   //   get a random index number 0 to 25
   const randomIndex = Math.round(Math.random() * 25);
-  return alphabetString[randomIndex];
+  return alphabets[randomIndex];
+}
+
+// decrement life
+function decLife() {
+  const currentLife = getTextElementValueById("life");
+  const updatedLife = currentLife - 1;
+  return updatedLife;
 }
